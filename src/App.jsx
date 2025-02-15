9cc78d5f6abc0d5ea4c88a48c212a6ff77cd5f3b
xkintaro <mustafatasal4242@gmail.com>
2025-02-15 08:08:30 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Kintaro from './components/Kintaro';
+import './assets/css/main.css';
+
+const App = () => {
+  return (
+    <div>
+      <Kintaro />
+    </div>
+  );
+};
+
+export default App;
